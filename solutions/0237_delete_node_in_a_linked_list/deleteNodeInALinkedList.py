4b0bc3e62f0410e9f52315d2dc841a3bfabef5a1
Rylie Nelson <rnelso2@co.pierce.wa.us>
2016-10-18 21:09:35 +0000

    whoops

--- solutions/0237_delete_node_in_a_linked_list/deleteNodeInALinkedList.py
+++ solutions/0237_delete_node_in_a_linked_list/deleteNodeInALinkedList.py
@@ -4,12 +4,6 @@ class ListNode(object):
         self.next = None
 
 def deleteNode(node):
-    # while node.next != None:
-    #     node.val = node.next.val
-    #     if node.next.next == None:
-    #         node.next = None
-    #     else:
-    #         node = node.next
     node.val = node.next.val
     node.next = node.next.next
     
